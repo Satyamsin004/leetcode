d808beadf8845b46f80274dfcd66271ba6c3c0c8
Satyam Kumar <satyamsin004@gmail.com>
2026-09-14 00:17:24 +0530

    Time: 1 ms (99.44%) | Memory: 46.7 MB (72.50%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.java
+++ 3778-transform-array-by-parity/transform-array-by-parity.java
@@ -3,18 +3,18 @@ class Solution {
         int n = nums.length;
         int count = 0;
 
-        for(int i = 0;i<n;i++){
-            if(nums[i] % 2 == 0){
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 0) {
                 count++;
             }
         }
-        for(int i = 0 ;i < count;i++){
+        for (int i = 0; i < count; i++) {
             nums[i] = 0;
         }
-        for(int i = count; i< n;i++){
+        for (int i = count; i < n; i++) {
             nums[i] = 1;
         }
         return nums;
-        
+
     }
 }
